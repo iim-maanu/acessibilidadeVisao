5d20b4cdd4e5548519dca21b43ebe1783ded0c0e
iim-maanu <emanueli.moraes.santos@escola.pr.gov.br>
2026-08-21 11:39:17 -0300

    ok

---
+++ script.js
@@ -0,0 +1,39 @@
+// Botões de acessibilidade
+document.addEventListener("DOMContentLoaded", () => {
+const btnContraste = document.getElementById("btn-contraste");
+const btnAumentar = document.getElementById("btn-aumentar-texto");
+const btnDiminuir = document.getElementById("btn-diminuir-texto");
+
+let tamanhoAtualFonte = 100;
+
+// Função alto contraste
+if (btnContraste) {
+btnContraste.addEventListener("click", () => {
+document.body.classList.toggle("alto-contraste");
+
+// Acessibilidade para leitores de tela
+const ativo = document.body.classList.contains("alto-contraste");
+btnContraste.setAttribute("aria-pressed", ativo);
+});
+}
+
+// Função aumentar texto
+if (btnAumentar) {
+btnAumentar.addEventListener("click", () => {
+if (tamanhoAtualFonte < 150) {
+tamanhoAtualFonte += 10;
+document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+}
+});
+}
+
+// Função diminuir texto
+if (btnDiminuir) {
+btnDiminuir.addEventListener("click", () => {
+if (tamanhoAtualFonte > 90) {
+tamanhoAtualFonte -= 10;
+document.documentElement.style.fontSize = `${tamanhoAtualFonte}%`;
+}
+});
+}
+});
